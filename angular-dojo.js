a919d027d5068da7251e26a2afad3f25d2406136
Amaury Decreme <amaury.decreme@gmail.com>
2014-02-21 14:10:10 +0100

    Event treatment improvment
    * $observe
    * Toggle button checked property

--- angular-dojo.js
+++ angular-dojo.js
@@ -2,80 +2,76 @@ if (!String.prototype.trim) {
    String.prototype.trim=function(){return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');};
 }
 
-function parseProps(props, scope) {
-    var result = {};
-    if (props != undefined) {
-        var propsArray = props.split(";");
-        angular.forEach(propsArray, function (prop, index) {
-            var propSplit = prop.split(":");
-            if (scope.$parent[propSplit[1].trim()]) {
-                result[propSplit[0].trim()] = scope.$parent[propSplit[1].trim()];
-            }else{
-                result[propSplit[0].trim()] = eval(propSplit[1].trim());            
-            }
-        });
-    }
-    return result;
-};
 
-angular.module('angular-dojo', []).
-directive('dojoWidget', function() {
+angular.module('angular-dojo', []).directive('dojoWidget', function() {
+    
+    var parseProps = function(props, scope) {
+        var result = {};
+        if (props != undefined) {
+            angular.forEach(props.split(";"), function (prop, index) {
+                var propSplit = prop.split(":");
+                if (scope.$parent[propSplit[1].trim()]) {
+                    result[propSplit[0].trim()] = scope.$parent[propSplit[1].trim()];
+                }else{
+                    result[propSplit[0].trim()] = eval(propSplit[1].trim());            
+                }
+            });
+        }
+        return result;
+    };
+    
     return {
         restrict: "A",
         replace: false,
         transclude: false,
         require: '?ngModel',
         scope: {
-            'ngModel' : '=',
+            'ngModel' : '=?',
             'ngClick' : '&',
             'ngChange' : '&',
             'dojoStore' : '&',
             'dojoProps' : '@',
-            'dojoDisplayValue' : '='
+            'dojoDisplayValue' : '=?'
         },
         link: function(scope, element, attrs, model) {
-            require(["dojo/ready", "dijit/dijit",
-                attrs.dojoWidget, "dojo/on"], function(ready, dijit, DojoWidget, on) {
-                var elem = angular.element(element[0]);
-                
-                ready(function () {
-                    var properties = {};
-                    if (attrs.dojoProps) {
-                        properties = parseProps(scope.dojoProps, scope);
-                    }
+            require(["dojo/ready", "dijit/dijit", attrs.dojoWidget, "dojo/on"], function(ready, dijit, DojoWidget, on) {
+              
+                ready(function () {                
+                    scope.widget = new DojoWidget({}, element[0]);
                     
-                    if (attrs.dojoStore) {
-                        properties.store = scope.dojoStore();
-                    };
+                    attrs.$observe('dojoProps', function(dojoProps) {
+                        scope.widget.set(parseProps(dojoProps, scope));
+                    });
                     
-                    properties.value = scope.ngModel;
-                
-                    scope.widget = new DojoWidget(properties, element[0]);
-
+                    attrs.$observe('dojoStore', function() {
+                        scope.widget.set('store', scope.dojoStore);
+                    });
+                    
+                    attrs.$observe('ngModel', function() {
+                        scope.widget.set('value', scope.ngModel);
+                        scope.widget.set('checked', scope.ngModel);
+                    });
+                    
                     on(scope.widget, "blur", function () {
-                        if (scope.widget.displayedValue) {
+                        if (scope.widget.displayedValue != undefined) {
                           scope.dojoDisplayValue = scope.widget.displayedValue;
                         }
                     });
 
                     on(scope.widget, "change", function(newValue) {
                         scope.ngModel = newValue;
-                        scope.$digest();
-                        if (scope.ngChange) {
+                        if (scope.ngChange != undefined) {
                             scope.ngChange();
                         }
                         scope.$apply();
                     });
 
-                    if (attrs.ngClick) {
-                        on(scope.widget, 'click', function() {
-                            if (scope.ngClick) {
-                                scope.ngClick();
-                            }
-                        });  
-                    }
-
-                    scope.ngModel = scope.widget.get('value');
+                    on(scope.widget, 'click', function() {
+                        if (scope.ngClick != undefined) {
+                            scope.ngClick();
+                            scope.$apply();
+                        }
+                    });
                 });
             });
         }
